fb92f31fa8abc51fc46867b802596f8f27131416
iaashis <aashishmaheswar.i@optum.com>
2017-06-26 15:17:34 +0530

    1st assignment components

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+Udemy Sample Project for Practice
+
 # TestAuth
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 1.0.0.
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+import { WarningComponent } from './warning/warning.component';
+import { SuccessComponent } from './success/success.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    WarningComponent,
+    SuccessComponent
   ],
   imports: [
     BrowserModule,
