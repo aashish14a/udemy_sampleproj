d09c3b355e40384fc42857a3b95025536b66ad1b
iaashis <aashishmaheswar.i@optum.com>
2017-07-02 12:55:49 +0530

    2nd assignment. Server component has the username reset button for the assignment

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,16 +2,33 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  template: `
-  <app-server></app-server>
-  <app-server></app-server>`,
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created';
+  serverName = '';
+  userName = '';
+  allowNewUsername = false;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
   }
 
+  ngOnInit() {
+    if (this.userName.length !== 0) {
+      this.allowNewUsername = true;
+    }
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created!';
+  }
+
+  allowUserclick() {
+    this.userName = '';
+  }
 }
